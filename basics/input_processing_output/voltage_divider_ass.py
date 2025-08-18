a372d302e5aa72c03c8bcce0372e3d0b405ce8de
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-18 22:10:10 +0000

    Add voltage_divider_ass.py

---
+++ basics/input_processing_output/voltage_divider_ass.py
@@ -0,0 +1,9 @@
+R1 = float(input('Enter the value for R1: '))
+
+R2 = float(input('Enter the value for R2: '))
+
+Vin =float(input('Enter the value of Vin: '))
+
+Vout = Vin * R2 / (R1 + R2)
+
+print('The value of Vout is: ', Vout)
