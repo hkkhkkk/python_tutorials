c4c1ce49dac4ea522b6023888b73d9afc2e23294
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-26 16:38:18 +0000

    Finding the average of numbers

---
+++ basics/input_processing_output/average.py
@@ -0,0 +1,16 @@
+# calculating the average of three numbers ;
+
+x = float(input('Enter your first number : '))
+
+
+y = float(input('Enter your second number : '))
+
+
+z = float(input('Enter your third number : '))
+
+
+Average = (x + y + z) / 3 
+
+
+print('Based on the numbers you entered, the average is :', Average)
+
