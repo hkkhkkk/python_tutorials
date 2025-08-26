2167368b17c07f230e7bf0cba0a169ef1c70cbcc
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-26 16:38:59 +0000

    Calculating simple interest

---
+++ basics/input_processing_output/simple_interest.py
@@ -0,0 +1,14 @@
+#user calculating for simple interest and need to present these key values outlined below:
+P = float(input('Input your principal amount , P: '))
+
+R = float(input('Input the Rate of Interest , R: '))
+
+
+T = float(input('Input the Time , T: '))
+
+# calculate Simple Interest is represented as SI  
+SI = (P * R * T) / 100
+
+
+print('The simple interest(%), based on the values you inputted will be: ', SI)
+
