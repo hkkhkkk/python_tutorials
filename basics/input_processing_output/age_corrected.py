c7d73724018eab58bbeba5eee990be7dcec84d06
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-26 22:35:02 +0000

    Add age_corrected.py

---
+++ basics/input_processing_output/age_corrected.py
@@ -0,0 +1,8 @@
+TIME_IN_YEARS = 10
+
+current_age = int(input('Input your current age: '))
+
+age_in_10_years = TIME_IN_YEARS + current_age
+
+print('Your age in 10 years will be : ', age_in_10_years)
+
