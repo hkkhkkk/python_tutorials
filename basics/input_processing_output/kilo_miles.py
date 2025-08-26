181698c717b1a567a90ddecd2839fa5f626c4aef
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-26 16:38:39 +0000

    Converting kilo to miles

---
+++ basics/input_processing_output/kilo_miles.py
@@ -0,0 +1,10 @@
+# converting kilometer to miles ;
+
+number = float(input('Enter value in km : '))
+
+
+miles = number / 1.609344
+
+
+print('Converting this value in km to miles will be  , :', miles)
+
