ebffd97498a75d6e94f45f61f2945e7caf108b2f
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-08-26 16:39:29 +0000

    Temperature conversion calculator

---
+++ basics/input_processing_output/temperature_converter.py
@@ -0,0 +1,11 @@
+# calculating the temperature in fahrenheit (F) ;
+
+C = float(input('Input your temperature in Celcius, C: '))
+
+
+F = (C * (9 / 5) + 32 ) 
+
+
+print('Your temperature in Fahrenheit (F), will be: ', F)
+
+
