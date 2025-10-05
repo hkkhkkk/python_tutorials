a52a883c046d30c2bd30763a846c8a5e357e8b2e
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-10-05 19:57:56 +0000

    Add apple_ii.py

---
+++ basics/input_processing_output/apple_ii.py
@@ -0,0 +1,10 @@
+number_of_apples_i_have = int(input('Number of apples I have : '))
+
+number_of_apples_i_received_from_my_friend = 7
+
+number_of_apples_i_ate = 3
+
+number_of_apples_left = (number_of_apples_i_have + number_of_apples_i_received_from_my_friend) - number_of_apples_i_ate
+
+print(f'Now I have {number_of_apples_left} apples')
+
