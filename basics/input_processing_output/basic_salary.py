76dc6a846ee0514484c7b4c808eb0963f929b5e0
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-10-10 20:01:17 +0000

    Add basic_salary.py

---
+++ basics/input_processing_output/basic_salary.py
@@ -0,0 +1,9 @@
+Basic_salary = float(input('Enter the value for Basic_salary: '))
+
+Allowances = float(input('Enter the value for Allowances: '))
+
+Deductions = float(input('Enter the value for Deductions: '))
+
+Net_Salary = (Basic_salary + Allowances - Deductions)
+
+print('Your Net Salary, based on the values you inputted will be: ', Net_Salary)
