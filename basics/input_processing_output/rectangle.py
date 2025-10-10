d370c71323eed1b1608e70ddae37578cde65d6bc
hkkhkkk <123324439+hkkhkkk@users.noreply.github.com>
2025-10-10 20:01:41 +0000

    Add rectangle.py

---
+++ basics/input_processing_output/rectangle.py
@@ -0,0 +1,13 @@
+CONSTANT = 2
+
+Length = float (input('Enter the value of Length: '))
+
+Width = float(input('Enter the value of Width: '))
+
+Area = (Length * Width)
+
+Perimeter = CONSTANT * (Length + Width)
+
+print('The Area will be: ', Area)
+
+print('The Perimeter will be: ', Perimeter)
